a6b49605e388aaa2a5d34a17e3bec9bcb1912f8e
Greg Sypolt <gsypolt@gsypoltmbpr.home>
2014-08-20 14:06:51 -0400

    fixed the spelling error

--- GGridiron/2014/_send_draft_pick_notification.php
+++ GGridiron/2014/_send_draft_pick_notification.php
@@ -4,7 +4,7 @@ authentication_handle_login();
 
 $this_franchise_id = authentication_get_current_franchise();
 if($this_franchise_id <= 0) {
-    json_print_error("Invalid franchises"); 
+    json_print_error("Invalid franchises");
     exit;
 }
 
@@ -12,15 +12,15 @@ $last_pick_details = get_current_pick_details(-1);
 
 if($this_franchise_id != (int)$last_pick_details['franchise_id']) {
     if(!is_franchise_commish($this_franchise_id) && REQUIRE_COMMISH_PRIV) {
-        json_print_error("Could not send drfat pick notifications since you are not a commish and you are not the drafting franchises"); 
+        json_print_error("Could not send draft pick notifications since you are not a commish and you are not the drafting franchises"); 
         exit;
     }
 }
 
 if(SendDraftPickNotification()) {
     json_print_success();
-    exit;   
+    exit;
 }
 
-json_print_error("Could not send draft pick notifications");   
+json_print_error("Could not send draft pick notifications");
 exit;
