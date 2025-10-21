86525ba7d87cae9a0e2269be5b690c97e68c79f2
Greg Sypolt <gsypolt@Gregs-MacBook-Pro.local>
2025-10-20 21:48:33 -0400

    minor tweak to add/drop page broken trash icon

--- legacy/ggridiron/draft-day-app/_get_current_franchise_id.php
+++
@@ -1,6 +0,0 @@
-<?php
-    header('Content-Type: application/json');
-    require_once "core.php";
-    
-    $franchise_id = authentication_get_current_franchise();
-    echo json_encode($franchise_id);
--- legacy/ggridiron/draft-day-app/_get_last_pick_timestamp.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Content-Type: application/json');
-    require_once "core.php";
-    
-    $timestamp = strtotime(get_last_pick_timestamp());
-    echo json_encode($timestamp);
-    exit;
